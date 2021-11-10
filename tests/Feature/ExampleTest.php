75e7e70f37876afdbe8da4ae174904518e3f72ea
Cstevenlopez <cstevenlopez2001@gmail.com>
2021-11-09 21:28:27 -0600

    Creacion de los de mas crud Store y index , guardar y ver

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -18,9 +18,9 @@ class ExampleTest extends TestCase
 
         $response->assertStatus(200);
     }
-    public function test_delete()
+/*    public function test_delete()
     {
-        $response = $this->delete('/api/proveedores/2');
+        $response = $this->delete('/api/proveedores/7');
 
         $response
             ->assertStatus(200)
@@ -30,7 +30,7 @@ class ExampleTest extends TestCase
     }
     public function test_update()
     {
-        $response = $this->put('/api/proveedores/3', ['nombres' => 'juan','apellidos' => 'Reyes','direccion' => 'libertad','ciudad' => 'Rosita','numero_cedula' => '165136Q','numero_telefono' => '56138318','terminos_pagos' => 'robo']);
+        $response = $this->put('/api/proveedores/3', ['nombres' => 'mario','apellidos' => 'Reyes','direccion' => 'libertad','ciudad' => 'Rosita','numero_cedula' => '165136Q','numero_telefono' => '56138318','terminos_pagos' => 'robo']);
 
         $response
             ->assertStatus(200)
@@ -38,4 +38,64 @@ class ExampleTest extends TestCase
                 'mensaje' => 'proveedor actualizado exitosamente',
             ]);
     }
+    public function test_store()
+    {
+        $response = $this->post('/api/proveedores', ['nombres' => 'tony','apellidos' => 'pito','direccion' => 'mierda','ciudad' => 'Rosita','numero_cedula' => '165136Q','numero_telefono' => '56138318','terminos_pagos' => 'robo']);
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'mensaje' => 'Proveedor guardado exitosamente',
+            ]);
+    }
+    public function test_index()
+    {
+        $response = $this->get('/api/proveedores');
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                
+            ]);
+    }*/
+ /* public function test_delete()
+    {
+        $response = $this->delete('/api/proveedores/10');
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'mensaje' => 'proveedor Eliminado exitosamente',
+            ]);
+    }*/
+/*  public function test_update()
+    {
+        $response = $this->put('/api/proveedores/3', ['nombres' => 'mario','apellidos' => 'Reyes','direccion' => 'libertad','ciudad' => 'Rosita','numero_cedula' => '165136Q','numero_telefono' => '56138318','terminos_pagos' => 'robo']);
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'mensaje' => 'proveedor actualizado exitosamente',
+            ]);
+    }*/
+    public function test_store()
+    {
+        $response = $this->postJson('/api/proveedores', ['nombres' => 'tony','apellidos' => 'pito','direccion' => 'mierda','ciudad' => 'Rosita','numero_cedula' => '165136Q','numero_telefono' => '56138318','terminos_pagos' => 'robo']);
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'mensaje' => 'Proveedor guardado exitosamente',
+            ]);
+    }
+  /*  public function test_index()
+    {
+        $response = $this->get('/api/proveedores');
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                
+            ]);
+    }*/
 }
